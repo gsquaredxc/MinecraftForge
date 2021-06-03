95519459d0cb8d73f203e225ad892c88704cd77f
ChachyDev <shalomadecoolboy@outlook.com>
2021-06-03 20:49:56 +0100

    Fix documentation of JavaUtils

--- src/main/java/net/minecraftforge/utils/JavaUtils.java
+++ src/main/java/net/minecraftforge/utils/JavaUtils.java
@@ -1,15 +1,9 @@
 package net.minecraftforge.utils;
 
 public class JavaUtils {
+    private final String javaVersion = System.getProperty("java.version");
     public static JavaUtils INSTANCE = new JavaUtils();
 
-    private final String javaVersion = System.getProperty("java.version");
-
-    /**
-     * Logic: In Java 9+ AppClassLoader is no longer extends URLClassLoader meaning we can check if the version by seeing if it still extends it or not
-     *
-     * @return Whether the current JRE is Java 8/Whatever extended URLClassLoader
-     */
     public boolean isJava8() {
         return javaVersion.startsWith("1.8");
     }
