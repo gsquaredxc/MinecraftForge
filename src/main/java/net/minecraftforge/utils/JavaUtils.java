5cc78a492e8b88985d1586e845faf4ae6eb181ab
ChachyDev <shalomadecoolboy@outlook.com>
2021-06-03 11:08:05 +0100

    Cleanup JavaUtils

--- src/main/java/net/minecraftforge/utils/JavaUtils.java
+++ src/main/java/net/minecraftforge/utils/JavaUtils.java
@@ -1,11 +1,9 @@
 package net.minecraftforge.utils;
 
-import net.minecraftforge.launch.ForgeLaunchWrapper;
-
-import java.net.URLClassLoader;
-
 public class JavaUtils {
     public static JavaUtils INSTANCE = new JavaUtils();
+
+    private final String javaVersion = System.getProperty("java.version", "1.8.0_52");
 
     /**
      * Logic: In Java 9+ AppClassLoader is no longer extends URLClassLoader meaning we can check if the version by seeing if it still extends it or not
@@ -13,10 +11,10 @@ public class JavaUtils {
      * @return Whether the current JRE is Java 8/Whatever extended URLClassLoader
      */
     public boolean isJava8() {
-        return ForgeLaunchWrapper.class.getClassLoader() instanceof URLClassLoader;
+        return javaVersion.startsWith("1.8");
     }
 
     public boolean isJava7() {
-        return System.getProperty("java.version", "1.8.0_251").startsWith("1.7");
+        return javaVersion.startsWith("1.7");
     }
 }
